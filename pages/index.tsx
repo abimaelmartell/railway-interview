1a58ec4bfcff1c58cd13143da4fec17b5fc4fa25
Abimael Martell <abimex@gmail.com>
2025-05-16 19:24:29 -0700

    move to index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,66 @@
-import { Geist, Geist_Mono } from 'next/font/google'
-import Link from 'next/link'
+import { GetServerSideProps } from 'next'
+import { useCallback, useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-})
+import {
+  fetchProjectsWithServices,
+  RailwayProjectsWithServicesData,
+} from '@/lib/gql/fetch-services'
 
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-})
+import WorkspaceList from '@/components/WorkspaceList'
+import LoadingIcon from '@/components/LoadingIcon'
+import { DEPLOYMENT_PENDING_STATUSES } from '@/lib/constants'
 
-export default function Home() {
+type Props = {
+  data: RailwayProjectsWithServicesData
+}
+
+const REFRESH_INTERVAL = 1000
+
+export default function Containers({ data }: Props) {
+  const router = useRouter()
+
+  const [isRefreshing, setIsRefreshing] = useState(false)
+
+  const deploymentPending = data.me.workspaces.some((w) =>
+    w.team.projects.edges.some((p) =>
+      p.node.services.edges.some((s) =>
+        s.node.deployments.edges.some((d) => DEPLOYMENT_PENDING_STATUSES.includes(d.node.status)),
+      ),
+    ),
+  )
+
+  const handleRefresh = useCallback(async () => {
+    setIsRefreshing(true)
+    router.replace(router.asPath).finally(() => setIsRefreshing(false))
+  }, [router])
+
+  useEffect(() => {
+    if (deploymentPending) {
+      setTimeout(() => {
+        handleRefresh()
+      }, REFRESH_INTERVAL)
+    }
+  }, [deploymentPending, handleRefresh])
+
+
   return (
-    <div
-      className={`${geistSans.className} ${geistMono.className} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <h1 className="text-4xl font-bold">Railway Interview Project</h1>
+    <div className="p-4 sm:p-6 md:p-8 max-w-3xl mx-auto bg-[var(--background)] text-[var(--foreground)]">
+      <div className="flex items-center justify-between mb-8">
+        <h1 className="text-3xl font-bold">🚀 Railway Containers</h1>
 
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Link
-          href="/containers"
-          className="text-blue-500 hover:text-blue-600 border border-blue-500 rounded-md p-2"
-        >
-          Go to container list
-        </Link>
-      </main>
+        {isRefreshing && (
+          <span className="text-sm text-gray-500 animate-pulse">
+            <LoadingIcon />
+          </span>
+        )}
+      </div>
+      <WorkspaceList data={data} onChange={handleRefresh} />
     </div>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const data = await fetchProjectsWithServices()
+  return { props: { data } }
 }
